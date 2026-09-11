4d81a119b2f3602d880973b38971f9cecb1f856a
Puneeth Raj Gorigam <punithraj400@gmail.com>
2026-09-11 13:11:18 +0530

    Test File

---
+++ CoreJavaPractice/src/com/java/lab/Arrays.java
@@ -0,0 +1,13 @@
+package com.java.lab;
+
+public class Arrays {
+	public static void main(String[] args) {
+		
+		int [] arr = { 10, 20, 30, 40, 50};
+		
+		for(int i : arr) {
+			System.out.println(i);
+		}
+	}
+
+}
